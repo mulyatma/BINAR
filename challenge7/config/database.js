63c016ae07560db153250c118c024a5dbee6ebaf
Mulya Tri Atmaja <mulyaatmaja604@gmail.com>
2023-11-19 20:52:16 +0700

    editting config database

--- challenge7/config/database.js
+++ challenge7/config/database.js
@@ -1,34 +1,34 @@
 const {
-  DB_USER = "",
-  DB_PASSWORD = "",
-  DB_NAME = "bcr",
-  DB_HOST = "127.0.0.1",
-  DB_PORT = "5432",
+  DB_USER = "postgres",
+  DB_PASSWORD = "2BE56D1BDBcGd2D*G5af2d31EG5gf36*",
+  DB_NAME = "railway",
+  DB_HOST = "roundhouse.proxy.rlwy.net",
+  DB_PORT = "50011",
 } = process.env;
 
 module.exports = {
   development: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_development`,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
   },
   test: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_test`,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
   },
   production: {
     username: DB_USER,
     password: DB_PASSWORD,
-    database: `${DB_NAME}_production`,
+    database: DB_NAME,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
-  }
-}
+    dialect: "postgres",
+  },
+};
